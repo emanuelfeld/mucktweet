813e48ba48232777c36ebbc17475c7cf67c537a1
emanuelfeld <emanuelfeld@gmail.com>
2017-10-06 22:12:32 -0400

    getting there

--- js/background.js
+++ js/background.js
@@ -1,110 +1,272 @@
 (function () {
   'use strict'
 
-  if (!!window.chrome) {
+  let DEBUG = false
+
+  if (window.chrome) {
     window.browser = window.chrome
   } else {
     window.browser = browser
   }
 
-  const browserStorage = window.browser.storage.local
-  window.browser.runtime.onMessage.addListener(handleMessage)
+  const DB_NAME = 'mucktweet-db'
+  const DB_VERSION = 1
+  const DB_USER_STORE_NAME = 'user'
+  const DB_TWEET_STORE_NAME = 'tweet'
 
-  function handleMessage (request, sender, sendResponse) {
-    if (request.update === true) {
-      let popup = new PopUp()
-      popup.updateUsers()
-      popup.updateTweets()
+  const timestampNow = Date.now()
+  let lastUpdate
+
+  let db
+  let localStorage = window.browser.storage.local
+  let badgeCounter = 0
+
+  window.browser.runtime.onMessage.addListener(handleReport)
+  window.browser.runtime.onConnect.addListener(handleDashboard)
+
+  openDb()
+
+  function handleDashboard (port) {
+    let tx = db.transaction([port.name], 'readwrite')
+    let store = tx.objectStore(port.name)
+
+    let modIndex = store.index('hasUpdate')
+    let modReq = modIndex.openCursor(IDBKeyRange.only(1))
+
+    modReq.onsuccess = function (evt) {
+      let cursor = evt.target.result
+      if (cursor) {
+        port.postMessage({'content': cursor.value})
+        cursor.continue()
+      }
+    }
+
+    let unmodIndex = store.index('status')
+    let unmodReq = unmodIndex.openCursor(IDBKeyRange.only('available'))
+
+    unmodReq.onsuccess = function (evt) {
+      let cursor = evt.target.result
+      if (cursor) {
+        port.postMessage({'content': cursor.value})
+        cursor.continue()
+      }
     }
   }
 
-  var PopUp = function () {
-    this.userDeletedCount = 0
-    this.userSuspendedCount = 0
-    this.userRemainsCount = 0
-    this.tweetDeletedCount = 0
-    this.tweetRemainsCount = 0
+  function handleReport (request, sender, sendResponse) {
+    if (request.type === 'report') {
+    let content = request.content
+    if (content.userData !== {}) {
+      let entry = queryDb(content.userData['id'], DB_USER_STORE_NAME)
+      if (entry === undefined) {
+        console.log('Adding user.')
+        entry = content.userData
+      } else {
+        console.warn('User already reported. Updating.')
+        entry['userReportCount']++
+        entry['hasUpdate'] = 0
+      }
+      addToDb(entry, DB_USER_STORE_NAME)
+    }
+
+    if (content.tweetData !== {}) {
+      let entry = queryDb(content.tweetData['id'], DB_TWEET_STORE_NAME)
+      if (entry === undefined) {
+        console.log('Adding tweet.')
+        addToDb(content.tweetData, DB_TWEET_STORE_NAME)
+      } else {
+        console.warn('Tweet already reported.')
+      }
+    }
+  } else if (request.type === 'download') {
+    downloadLocal(request.content)
+  }
+}
+  function updateStatus (force = false) {
+    console.log('updating badge')
+    localStorage.get({
+      'mucktweetLastUpdate': 0
+    }, function (res) {
+      lastUpdate = res.mucktweetLastUpdate
+      if (DEBUG === true || force === true ||
+         lastUpdate === 0 || (timestampNow > lastUpdate + 8.64e+7)) {
+        updateUserStatus()
+        updateTweetStatus()
+      }
+      localStorage.set({
+        'mucktweetLastUpdate': timestampNow
+      })
+    })
   }
 
-  PopUp.prototype = {
-    numUpdates: function () {
-      return this.userDeletedCount +
-             this.userSuspendedCount +
-             this.tweetDeletedCount
-    },
+  function updateBadge () {
+    localStorage.set({
+      'badgeCounter': badgeCounter
+    })
+    window.browser.browserAction.setBadgeText({
+      text: badgeCounter.toString()
+    })
+  }
 
-    updateBadge: function () {
-      const badgeText = this.numUpdates().toString()
-      window.browser.browserAction.setBadgeText({
-        text: badgeText
-      })
-    },
+  function updateUserStatus () {
+    let tx = db.transaction(DB_USER_STORE_NAME, 'readwrite')
+    let userStore = tx.objectStore(DB_USER_STORE_NAME)
+    let userIndex = userStore.index('status')
+    let userReq = userIndex.openCursor()
 
-    updateUsers: function () {
-      let self = this
-      browserStorage.get({'taUsers': '{}'}, function (data) {
-        let users = JSON.parse(data.taUsers)
-        let usersSuspended = {}
-        let usersDeleted = {}
-
-        Object.keys(users).forEach(function (userId) {
-          // if (users[userId]['status'] === null) {
-            // fetch('https://twitter.com/intent/user?user_id=' + userId)
-              // .then(function (res) {
-                // if (res.url === 'https://twitter.com/account/suspended') {
-                  users[userId]['status'] = 'suspended'
-                  usersSuspended[userId] = users[userId]
-                  self.userSuspendedCount++
-                // } else if (res.status === 404) {
-                  // users[userId]['status'] = 'deleted'
-                  usersDeleted[userId] = users[userId]
-                  self.userDeletedCount++
-                // } else {
-                  // userRemainsCount++
-                // }
-              // })
-          // }
-        })
-
-        browserStorage.set({
-          'taUsers': JSON.stringify(users),
-          'taUsersSuspended': JSON.stringify(usersSuspended),
-          'taUsersDeleted': JSON.stringify(usersDeleted)
-        }, function () {
-          self.updateBadge()
-        })
-      })
-    },
-
-
-    updateTweets: function () {
-      let self = this
-      browserStorage.get({'taTweets': '{}'}, function (data) {
-        let tweets = JSON.parse(data.taTweets)
-        let tweetsDeleted = {}
-
-        Object.keys(tweets).forEach(function (tweetId) {
-          // if (tweets[tweetId]['status'] === null) {
-            // fetch('https://twitter.com' + tweets[tweetId]['tweetPermalinkPath'])
-              // .then(function (res) {
-                // if (res.status === 404) {
-                  tweets[tweetId]['status'] = 'deleted'
-                  tweetsDeleted[tweetId] = tweets[tweetId]
-                  self.tweetDeletedCount++
-                // } else {
-                  self.tweetRemainsCount++
-                // }
-              // })
-          // }
-        })
-
-        window.browser.storage.local.set({
-          'taTweets': JSON.stringify(tweets),
-          'taTweetsDeleted': JSON.stringify(tweetsDeleted)
-        }, function () {
-          self.updateBadge()
-        })
-      })
+    userReq.onsuccess = function (evt) {
+      let cursor = evt.target.result
+      if (cursor && cursor.value.reportDate > lastUpdate) {
+        fetch('https://twitter.com/intent/user?user_id=' + cursor.value.id)
+            .then(function (res) {
+              if (res.url === 'https://twitter.com/account/suspended') {
+                cursor.value.status = 'suspended'
+                cursor.value.hasUpdate = 1
+                cursor.value.updatDate = timestampNow
+                addToDb(cursor.value, DB_USER_STORE_NAME)
+                updateBadge(badgeCounter++)
+              } else if (res.status === 404) {
+                cursor.value.status = 'deleted'
+                cursor.value.hasUpdate = 1
+                cursor.value.updatDate = timestampNow
+                addToDb(cursor.value, DB_USER_STORE_NAME)
+                updateBadge(badgeCounter++)
+              }
+            })
+        cursor.continue()
+      } else if (cursor) {
+        cursor.value.hasUpdate = 0
+        addToDb(cursor.value, DB_USER_STORE_NAME)
+        cursor.continue()
+      }
     }
+  }
+
+  function downloadLocal (storeName) {
+    let tx = db.transaction(storeName, 'readonly')
+    let store = tx.objectStore(storeName)
+    let req = store.openCursor()
+    let out = []
+    req.onsuccess = function (evt) {
+      let cursor = evt.target.result
+      if (cursor) {
+        out.push(cursor.value)
+        cursor.continue()
+      } 
+    }
+
+    tx.oncomplete = function () {
+      downloadData(out, storeName)
+    }
+  }
+
+  function downloadData (content, filename) {
+    let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(content, null, 4));
+    let dlAnchorElem = document.createElement('a')
+    dlAnchorElem.setAttribute("href", dataStr     )
+    dlAnchorElem.setAttribute("download", filename + ".json")
+    dlAnchorElem.click()
+  }
+
+  function updateTweetStatus () {
+    let tx = db.transaction(DB_TWEET_STORE_NAME, 'readwrite')
+    let tweetStore = tx.objectStore(DB_TWEET_STORE_NAME)
+    let tweetIndex = tweetStore.index('status')
+    let tweetReq = tweetIndex.openCursor()
+
+    tweetReq.onsuccess = function (evt) {
+      console.log('tweetReq success')
+      let cursor = evt.target.result
+      if (cursor && cursor.value.reportDate > lastUpdate) {
+        fetch('https://twitter.com' + cursor.value.permalinkPath)
+              .then(function (res) {
+                if (res.status === 404) {
+                  cursor.value.status = 'deleted'
+                  cursor.value.hasUpdate = 1
+                  cursor.value.updatDate = timestampNow
+                  addToDb(cursor.value, DB_TWEET_STORE_NAME)
+                  updateBadge(badgeCounter++)
+                }
+              })
+        cursor.continue()
+      } else if (cursor) {
+        cursor.value.hasUpdate = 0
+        addToDb(cursor.value, DB_USER_STORE_NAME)
+        cursor.continue()
+      }
+    }
+  }
+
+  function openDb () {
+    console.log('Opening MuckTweet DB.')
+    let req = indexedDB.open(DB_NAME, DB_VERSION)
+
+    req.onupgradeneeded = function (evt) {
+      console.log('Upgrading MuckTweet DB.')
+      db = this.result
+
+      if (!db.objectStoreNames.contains('user')) {
+        console.log('Making the user object store.')
+        let objectStore = db.createObjectStore('user',
+          { keyPath: 'id' })
+        objectStore.createIndex('status', 'status')
+        objectStore.createIndex('hasUpdate', 'hasUpdate')
+      }
+
+      if (!db.objectStoreNames.contains('tweet')) {
+        console.log('Making the tweet object store.')
+        let objectStore = db.createObjectStore('tweet',
+          { keyPath: 'id' })
+        objectStore.createIndex('status', 'status')
+        objectStore.createIndex('userTweets', 'userId')
+        objectStore.createIndex('hasUpdate', 'hasUpdate')
+      }
+    }
+
+    req.onsuccess = function (evt) {
+      console.log('Opened MuckTweet DB.')
+      db = this.result
+      updateStatus()
+    }
+
+    req.onerror = function (evt) {
+      console.error('Failed to open MuckTweet DB.')
+      console.error(this.error)
+    }
+  }
+
+  function addToDb (entry, storeName) {
+    console.log('Adding ID ' + entry['id'] + ' to ' + storeName + '.')
+    let store = getObjectStore(storeName, 'readwrite')
+    let req = store.put(entry)
+
+    req.onsuccess = function (evt) {
+      console.log('Successfully added ID ' + entry['id'] + ' to ' + storeName + '.')
+    }
+
+    req.onerror = function (evt) {
+      console.error('Failed to add ID ' + entry['id'] + ' to ' + storeName + '.')
+      console.error(this.error)
+    }
+  }
+
+  function queryDb (key, storeName) {
+    console.log('Querying ' + storeName + ' for ID ' + key + '.')
+    let store = getObjectStore(storeName, 'readwrite')
+    let req = store.get(key)
+
+    req.onsuccess = function (evt) {
+      return evt.target.result
+    }
+
+    req.onerror = function (evt) {
+      console.error('Failed to query ' + storeName + ' for ID ' + key + '.')
+      console.error(this.error)
+    }
+  }
+
+  function getObjectStore (storeName, mode) {
+    let tx = db.transaction(storeName, mode)
+    return tx.objectStore(storeName)
   }
 })()
--- js/main.js
+++ js/main.js
@@ -1,120 +1,88 @@
 (function () {
   'use strict'
 
-  var Report = function () {
-    this.userId = null
-    this.userData = {}
-    this.tweetId = null
-    this.tweetData = {}
+  let DEBUG = true
+
+  if (window.chrome) {
+    window.browser = window.chrome
+  } else {
+    window.browser = browser
   }
 
-  Report.prototype = {
-    getUserData: function (e) {
-      this.userId = e.getAttribute('data-user-id')
-      this.userData = {
-        'userScreenName': e.getAttribute('data-screen-name'),
-        'userName': e.getAttribute('data-name'),
-        'userStatus': null,
-        'userReportCount': 1 
-      }
-    },
+  let userData = {}
+  let tweetData = {}
 
-    getTweetData: function (e) {
-      this.tweetId = e.getAttribute('data-tweet-id')
-      this.tweetData = {
-        'userId': e.getAttribute('data-user-id'),
-        'tweetPermalinkPath': e.getAttribute('data-permalink-path'),
-        'tweetReportDate': parseInt(new Date().toJSON().slice(0, 10).replace(/-/g, '')),
-        'tweetStatus': null
-      }
-    },
-
-    beganTweetReport: function () {
-      return document.getElementById('report-dialog').classList.contains('report-tweet')
-      // return e.parentElement.classList.contains('report-link')
-    },
-
-    beganUserReport: function () {
-      return document.getElementById('report-dialog').classList.contains('report-user')
-      // return e.parentElement.classList.contains('report-text')
-    },
-
-    submittedReport: function (e) {
-      return e.classList.contains('new-report-flow-done-button')
+  function getUserData (node) {
+    return {
+      'id': node.getAttribute('data-user-id'),
+      'screenName': node.getAttribute('data-screen-name'),
+      'name': node.getAttribute('data-name'),
+      'status': 'suspended',
+      'reportCount': 1,
+      'reportDate': Date.now(),
+      'hasUpdate': 1,
+      'updateDate': null
     }
   }
 
-  function resetStorage (browserStorage) {
-    browserStorage.remove(['taLastUpdate', 'taTweets', 'taUsers'], function () {
-      let error = window.browser.runtime.lastError
-      if (error) {
-        console.error(error)
-      }
-    })
+  function getTweetData (node) {
+    return {
+      'id': node.getAttribute('data-tweet-id'),
+      'userId': node.getAttribute('data-user-id'),
+      'permalinkPath': node.getAttribute('data-permalink-path'),
+      'status': 'available',
+      'postDate': parseInt(node.querySelector('.js-relative-timestamp').getAttribute('data-time-ms')),
+      'reportDate': Date.now(),
+      'hasUpdate': 0,
+      'updateDate': null
+    }
   }
 
-  window.onload = function () {
-    let report = new Report()
-
-    if (!!window.chrome) {
-      window.browser = window.chrome
-    } else {
-      window.browser = browser
+  function beganReport (node, reportType) {
+    let reportActive = document.getElementById('report-dialog') !== null &&
+      document.getElementById('report-dialog')
+        .style.display !== 'none' &&
+      document.getElementById('report-dialog')
+        .classList.contains('report-' + reportType)
+    if (reportActive &&
+        document.getElementById('report-dialog')
+          .contains(node) &&
+        document.getElementById('report-dialog')
+          .querySelector('.modal-content')
+          .contains(node) === false) {
+      return false
     }
+    return reportActive
+  }
 
-    let browserStorage = window.browser.storage.local
+  function submittedReport (node) {
+    return node.classList.contains('new-report-flow-done-button')
+  }
 
-    // Check for status updates once a day
-    browserStorage.get({
-      'taLastUpdate': 0
-    }, function (res) {
-      let lastUpdate = res.taLastUpdate
-      // if (lastUpdate === 0 || (todayDate > lastUpdate)) {
-        window.browser.runtime.sendMessage({
-          'update': true
-        })
-      // }
-    })
+  function hasReportData () {
+    return userData !== {} || tweetData !== {}
+  }
 
-    document.onclick = function (event) {
-      if (report.beganUserReport()) {
-        let elem = document.querySelector('.user-actions')
-        report.getUserData(elem)
-        console.log(report)
-      } else if (report.beganTweetReport()) {
-        let elem = event.srcElement.closest('.tweet')
-        report.getUserData(elem)
-        report.getTweetData(elem)
-        console.log(report)
-      } else if (report.submittedReport(event.srcElement)) {
-        browserStorage.get({
-          'taTweets': '{}',
-          'taUsers': '{}'
-        }, function (res) {
-          let tweets = JSON.parse(res.taTweets)
-          let users = JSON.parse(res.taUsers)
-
-          if (report.tweetId && !(report.tweetId in tweets)) {
-            tweets[report.tweetId] = report.tweetData
-            browserStorage.set({
-              'taTweets': JSON.stringify(tweets)
-            })
-          }
-
-          if (report.userId && !(report.userId in users)) {
-            users[report.userId] = report.userData
-          } else {
-            users[report.userId]['userReportCount'] += 1
-          }
-
-          console.log(users)
-          console.log(tweets)
-
-          browserStorage.set({
-            'taUsers': JSON.stringify(users)
-          })
-        })
-      }
+  document.onclick = function (evt) {
+    if (beganReport(evt.srcElement, 'user')) {
+      const node = document.querySelector('.user-actions')
+      userData = getUserData(node)
+      console.log('Reporting user: ' + JSON.stringify(userData))
+    } else if (beganReport(evt.srcElement, 'tweet')) {
+      const node = evt.srcElement.closest('.tweet')
+      userData = getUserData(node)
+      console.log('Reporting user: ' + JSON.stringify(userData))
+      tweetData = getTweetData(node)
+      console.log('Reporting tweet: ' + JSON.stringify(tweetData))
+    } else if ((DEBUG && hasReportData()) || submittedReport(evt.srcElement)) {
+      console.log('Submitting report.')
+      window.browser.runtime.sendMessage({
+        'type': 'report',
+        'content': {
+          'userData': userData,
+          'tweetData': tweetData
+        }
+      })
     }
   }
 })()
